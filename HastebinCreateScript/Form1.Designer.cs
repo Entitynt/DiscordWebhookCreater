72676d8b48a4c5c3d6edd9d8cb4f67cd3f1cdc6d
Entity <75163577+EntityTR@users.noreply.github.com>
2023-01-09 21:26:55 +0300

    hey!
    I used the old interface project because I was lazy and you need download Discord.webhook nuget

---
+++ HastebinCreateScript/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace DiscordWebhookCreate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(134, 149);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(522, 188);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(175, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Discord Webhook Create 2023!1!!1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 81);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create Webhook Now!!1!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "DiscordWebhookCreate!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HastebinCreateScript/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Webhook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscordWebhookCreate
+{
+    public partial class Form1 : Form
+    {
+        string webhookUrl = "https://discord.com/api/webhooks/1062066687412817960/_A6N0xi2hM0-4K0LnGNahmX4KzYm6H4-kEyRer_bBX9gbQRMu-liqZt9P-OnlB2neeaj";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a new WebhookClient with the WebhookUrl
+            DiscordWebhookClient client = new DiscordWebhookClient(webhookUrl);
+
+            // Send the message to the webhook
+            client.SendMessageAsync(richTextBox1.Text);
+        }
+    }
+}
